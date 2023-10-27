7d3844be6568fbbf993f384f69a23d0cf04006b9
jjyaman <juanjosy1972@gmail.com>
2023-10-27 15:40:24 -0500

    Comparación real-time y registros base de datos

--- srf_v9.py
+++ srf_v9.py
@@ -15,6 +15,9 @@ db.connect()
 data = db.select_column_from_table('rostro', 'visitante')
 
 # Creamos una lista que va a almacenar todos los vectores embedding
+embedding_realtime = []
+
+umbral = 0.5
 
 # Iniciamos la captura de frames en tiempo real
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -22,20 +25,24 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 # Determinamos si se encuentra una cara en el frame capturado
 detector = dlib.get_frontal_face_detector()
 
+cont = 0
+
 while True:
+
     ret, frame = cap.read()
     if ret == False:
         break
     
-     # Convertimos la imagen a escala de grises
+    # Convertimos la imagen a escala de grises
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
 
     if faces is not None:
-
+        
         # Extraemos las coordenadas y dimensiones del rostro
         for face in faces:
+            
             x, y, w, h = face.left(), face.top(), face.width(), face.height()
             
             # Redimensionamos el rostro
@@ -44,7 +51,26 @@ while True:
             # Vector embedding con face-recognition
             embedding = face_recognition.face_encodings(face)[0]
 
+            # embedding_realtime.append(embedding)
+
+            cont += 1
+
+            for i in data:
+
+                # Convertimos el vector a su formato original
+                hexadecimal = i[0].decode('utf-8')
+                byte_array = bytearray.fromhex(hexadecimal)
+                embedding_db = struct.unpack("f" * (len(byte_array) // 4), byte_array)
+
+                # Calculamos la distancia euclidiana
+                distancia = np.linalg.norm(embedding - embedding_db)
+
+                if distancia <= umbral:
+                    print("Sí")
+                else:
+                    print("No")
             
+
 
     else:
         print("No se detecto ningún rostro")
@@ -52,7 +78,7 @@ while True:
     cv2.imshow('frame', frame)
 
     k = cv2.waitKey(1)
-    if k == 27:
+    if k == 27 or cont == 2:
         break
 
 # Cerramos la conexión con la Base de Datos
